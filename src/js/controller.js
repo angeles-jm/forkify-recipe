b75d3dd018b905740fad8f43fd6fa140342e9850
angeles-jm <angeles.johnmark07@gmail.com>
2024-05-06 17:14:58 +0800

    added a new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -137,6 +137,10 @@ const controlAddRecipe = async function (newRecipe) {
   // Upload the new recipe data
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -145,7 +149,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('welcome');
+  newFeature();
 };
 
 init();
